b16c351bb8f054931189253387500c46505ff718
Timasuper05 <76902286+Timasuper05@users.noreply.github.com>
2021-02-07 17:53:12 +0300

    Добавлен проект

---
+++ Test/Test/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PageCon.Content = new WelcomPage();
+        }
+
+        private void Home_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Home;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Главная";
+        }
+
+        private void Home_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Lessons_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = Lessons;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Уроки";
+        }
+
+        private void Lessons_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void About_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = About;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "О программе";
+        }
+
+        private void About_MouseLeave(object sender, MouseEventArgs e)
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            PageCon.Content = new WelcomPage();
+        }
+
+        private void Lessons_Click(object sender, RoutedEventArgs e)
+        {
+            PageCon.Content = new LessonsPage();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            PageCon.Content = new AboutPage();
+        }
+    }
+}
---
+++ Test/Test/VideoPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for VideoPage.xaml
+    /// </summary>
+    public partial class VideoPage : Window
+    {
+        DispatcherTimer timer;
+        public VideoPage()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Slider_seek.Value = Media.Position.TotalSeconds;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.Position = TimeSpan.FromSeconds(Slider_seek.Value);
+        }
+
+        private void Btn_Play_Click(object sender, RoutedEventArgs e)
+        {
+            Media.Play();
+        }
+
+        private void Btn_Pause_Click(object sender, RoutedEventArgs e)
+        {
+            Media.Pause();
+        }
+
+        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Media.Stop();
+        }
+
+        private void Slider_vol_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Media.Volume = (double)Slider_vol.Value;
+        }
+
+        private void Media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            TimeSpan ts = Media.NaturalDuration.TimeSpan;
+            Slider_seek.Minimum = ts.TotalSeconds;
+            timer.Start();
+        }
+    }
+}
